e818f0eba6b5a1df46e64ac19c6b7e0f410d731e
huykirito1201 <huyyhuqqho@gmail.com>
2022-12-17 10:13:23 +0700

    change

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("component");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -27,8 +29,17 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
